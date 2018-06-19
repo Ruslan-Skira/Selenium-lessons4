1c9e09361cd4d941141d68bcd154525c9c9cde0c
Oleg <oleg.kh81@gmail.com>
2018-06-19 10:19:49 +0300

    testAddAddress-> addAddress improve

--- src/test/java/com/academy/automation/practice/ddt/test/AddressTest.java
+++ src/test/java/com/academy/automation/practice/ddt/test/AddressTest.java
@@ -2,6 +2,8 @@ package com.academy.automation.practice.ddt.test;
 
 import com.academy.automation.practice.ddt.manager.model.AddressData;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.util.HashMap;
@@ -10,36 +12,43 @@ import java.util.List;
 
 public class AddressTest extends BaseTest {
 
-    @Test
-    // TODO
-    public void testAddAddress() {
-        AddressData addressData = new AddressData()
-                .withFirstName("Kolya")
-                .withLastName("Ivanov")
-                .withAddress("Petrovskogo st. 35")
-                .withCity("Kharkov")
-                .withState("Alaska")
-                .withZipCode("61033")
-                .withCountry("United States")
-                .withHomePhone("+3809353613437")
-                .withMobilePhone("093234567")
-                .withAddressAlias("addressRef");
-
+    @BeforeMethod
+    public void prepare() {
         manager.goTo().home();
         manager.session().login();
         manager.goTo().address();
-        if (manager.address().isPresentAlias(addressData.getAlias())) {
-            manager.address().removeAddress(addressData.getAlias());
+    }
+
+    @Test(dataProvider = "creationAddress")
+    public void testAddAddress(AddressData address) {
+        if (manager.address().isPresentAlias(address.getAlias())) {
+            manager.address().removeAddress(address.getAlias());
         }
 
         List<AddressData> beforeListAddr = manager.address().getAddresses();
-
-        manager.address().create(addressData);
+        manager.address().create(address);
 
         // verify
         List<AddressData> afterListAddr = manager.address().getAddresses();
         Assert.assertEquals(afterListAddr.size(), beforeListAddr.size()+1);
-        beforeListAddr.add(addressData.withAddressAlias(addressData.getAlias().toUpperCase()));
+        beforeListAddr.add(address.withAddressAlias(address.getAlias().toUpperCase()));
         Assert.assertEquals(new HashSet<>(beforeListAddr), new HashSet<>(afterListAddr));
+    }
+
+    @DataProvider(name="creationAddress")
+    private Object[] getCreationAddressData() {
+        return new Object[]{
+                new AddressData()
+                        .withFirstName("Kolya")
+                        .withLastName("Ivanov")
+                        .withAddress("Petrovskogo st. 35")
+                        .withCity("Kharkov")
+                        .withState("Alaska")
+                        .withZipCode("61033")
+                        .withCountry("United States")
+                        .withHomePhone("+3809353613437")
+                        .withMobilePhone("093234567")
+                        .withAddressAlias("addressRef")
+        };
     }
 }

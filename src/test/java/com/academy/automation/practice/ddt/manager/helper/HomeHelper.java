ec184c88040d16471b06d0ba03841f935b46bd10
Oleg <oleg.kh81@gmail.com>
2018-06-22 08:21:47 +0300

    automation: homeHelper added

---
+++ src/test/java/com/academy/automation/practice/ddt/manager/helper/HomeHelper.java
@@ -0,0 +1,4 @@
+package com.academy.automation.practice.ddt.manager.helper;
+
+public class HomeHelper {
+}
--- src/test/java/com/academy/automation/practice/ddt/test/HomeTests.java
+++ src/test/java/com/academy/automation/practice/ddt/test/HomeTests.java
@@ -5,14 +5,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 public class HomeTests {
 
